d3b60b25dd1976aca2b25b86ad28bbe94c67505e
DaEchteBasti <151942335+DaEchteBasti@users.noreply.github.com>
2023-11-24 15:32:57 +0100

    Add files via upload

---
+++ 02_projekt/js/script.js
@@ -0,0 +1,9 @@
+// Navigation Function
+function FunctionBurgerNav() {
+  var element = document.getElementById("main-nav");
+  if (element.classList.contains("expand")) {
+    element.classList.remove("expand");
+  } else {
+    element.classList.add("expand");
+  }
+}
